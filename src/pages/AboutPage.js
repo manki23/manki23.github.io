56d02cf98c0562fff70c7cf8f010634ccb251e7c
Myriam <manki@student.42.fr>
2021-09-08 15:23:43 +0200

    MOD: about page > wip

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/pages/AboutPage.scss'
+
+const AboutPage = () => {
+    return (
+      <div className="about-page">
+          <div className="title" >
+              <div className="number">01.</div>
+              <div className="content">About me</div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <p>Here are a few technologies I’ve been working with recently:</p>
+          <ol>
+              <li>Laravel</li>
+              <li>Typescript</li>
+              <li>Javascript</li>
+              <li>React</li>
+          </ol>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
