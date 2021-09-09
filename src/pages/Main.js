5cfc22aa119f426a9900cf19bbfd792bbbdfadd1
Myriam <manki@student.42.fr>
2021-09-09 14:53:51 +0200

    ADD: AboutPage.scss

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/pages/Main.scss'
+import AboutPage from './AboutPage';
 import HomePage from './HomePage';
 
 const Main = () => {
     return (
       <div className="main-content">
           <HomePage />
+          <AboutPage />
       </div>
     );
   };
