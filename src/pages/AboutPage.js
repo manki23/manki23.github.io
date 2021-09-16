28615bb4883835e1915cb087b8476eb62cc8ca0f
Myriam <manki@student.42.fr>
2021-09-16 16:16:56 +0200

    MOD: manki bitmoji changed for a centered one, about page position done, now missing animations and colors

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,26 +1,41 @@
-import React from 'react'
-import '../styles/pages/AboutPage.scss'
-import img from '../assets/manki-bitmoji.png'
+import React from "react";
+import "../styles/pages/AboutPage.scss";
+import img from "../assets/manki-bitmoji.png";
 
 const AboutPage = () => {
-    return (
-      <div className="about-page">
-          <div className="title" >
-              <div className="number">01.</div>
-              <div className="content">About me</div>
-          </div>
-          <img src={img}/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-          <p>Here are a few technologies I’ve been working with recently:</p>
+  return (
+    <div className="about-page">
+      <h2 className="numbered-heading">About me</h2>
+      <div className="content">
+        <div className="text-container">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum."
+          </p>
+          <p>
+            Here are a few technologies I’ve been working with recently:
+          </p>
           <ol>
-              <li>Laravel</li>
-              <li>Typescript</li>
-              <li>Javascript</li>
-              <li>React</li>
+            <li>Laravel</li>
+            <li>Angular Ionic</li>
+            <li>Javascript</li>
+            <li>React</li>
           </ol>
+        </div>
+        <div className="picture-container">
+          <img className="test-img" src={img} />
+        </div>
       </div>
-    );
-  };
-  
-  export default AboutPage;
+    </div>
+  );
+};
+
+export default AboutPage;
