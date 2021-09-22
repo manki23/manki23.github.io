17c596c33a1f10b8b4a1824c27d22e6d94c35247
Myriam <manki@student.42.fr>
2021-09-22 17:03:19 +0200

    ADD: Header.js > resume link (and file in assets)

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import '../../styles/shared/Header.scss'
 import GreenButton from './GreenButton';
 import HeaderButton from './HeaderButton';
+import cv from '../../assets/CV_2021-09-22_Myriam_Anki.pdf';
 
 const Header = () => {
     return (
       <header className="header-class">
-          <GreenButton title="Resume" href="#resume" />
+          <GreenButton title="Resume" href={cv} />
           <HeaderButton title="Contact" goTo="#contact-page" />
           <HeaderButton title="Work" goTo="#work-page" />
           <HeaderButton title="Experience" goTo="#experience-page" />
