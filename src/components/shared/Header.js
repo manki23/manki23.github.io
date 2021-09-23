7603a50081b29e007d3510df77d5c7e52495f5c8
Myriam <manki@student.42.fr>
2021-09-23 15:19:37 +0200

    MOD: Header.js > use of styled components

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import '../../styles/shared/Header.scss'
+import styled from 'styled-components'
 import GreenButton from './GreenButton';
 import HeaderButton from './HeaderButton';
 import cv from '../../assets/CV_2021-09-22_Myriam_Anki.pdf';
 
+const StyledHeader = styled.header`
+    display: flex;
+    flex-direction: row-reverse;
+    position: fixed;
+    right: 0;
+    margin-right: 40px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-family: monospace;
+    font-size: 16px;
+    z-index: 2;
+    top: 0;
+    width: 100vw;
+    height: 80px;
+    background-color: $appNavyColor;
+    opacity: 0.97;
+`
+
 const Header = () => {
     return (
-      <header className="header-class">
+      // <header className="header-class">
+      <StyledHeader>
           <GreenButton title="Resume" href={cv} />
           <HeaderButton title="Contact" goTo="#contact-page" />
           <HeaderButton title="Work" goTo="#work-page" />
           <HeaderButton title="Experience" goTo="#experience-page" />
           <HeaderButton title="About" goTo="#about-page"/>
-      </header>
+      </StyledHeader>
     );
   };
   
