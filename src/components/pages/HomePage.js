4dd7d4efd745ccf7fb80cee8d6fbdd943ba0b654
Myriam <manki@student.42.fr>
2021-09-23 17:12:18 +0200

    MOD: HomePage.js > use of styled-components instead of scss

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,18 +1,48 @@
-import React from 'react'
-import '../../styles/pages/HomePage.scss'
+import React from 'react';
+import styled from "styled-components";
+
+const StyledHomePage = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    #pre-title {
+        color: ${(props) => props.theme.colors.appGreenColor};
+        font-size: 20px;
+    }
+    
+    #title-name {
+        color: ${(props) => props.theme.colors.appLightestSlateColor};
+    }
+    
+    #title-whoami {
+        color: ${(props) => props.theme.colors.appLightSlateColor};
+    }
+    
+    #title-name, #title-whoami {
+        font-size: 70px;
+        font-weight: bold;
+    }
+
+    #small-intro {
+        color: ${(props) => props.theme.colors.appSlateColor};
+        margin-top: 20px;
+        font-size: 20px;
+    }
+`
 
 const HomePage = () => {
     return (
-      <div className="home-page" id="home-page">
-          <div className="pre-title" >
+      <StyledHomePage id="home-page">
+          <div id="pre-title" >
               Hi, my name is
           </div>
-          <div className="title-name">Myriam Anki.</div>
-          <div className="title-whoami">I am a freelance developper.</div>
-          <div className="small-intro">
+          <div id="title-name">Myriam Anki.</div>
+          <div id="title-whoami">I am a freelance developper.</div>
+          <div id="small-intro">
             I am a Laravel backend developper with some frontend skills in React.
           </div>
-      </div>
+      </StyledHomePage>
     );
   };
   
