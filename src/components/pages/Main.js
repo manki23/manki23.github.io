5152dc50873460d1c666d5b9d9325222156510c8
Myriam <manki@student.42.fr>
2021-09-23 17:15:39 +0200

    MOD: Main.js > use of styled-components instead of scss

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import '../../styles/pages/Main.scss'
-import AboutPage from './AboutPage';
-import HomePage from './HomePage';
-import ExperiencePage from './ExperiencePage'
-import WorkPage from './WorkPage';
-import ContactPage from './ContactPage';
+import React from "react";
+import styled from "styled-components";
+import AboutPage from "./AboutPage";
+import HomePage from "./HomePage";
+import ExperiencePage from "./ExperiencePage";
+import WorkPage from "./WorkPage";
+import ContactPage from "./ContactPage";
+
+const StyledMain = styled.div`
+  padding: 0px 150px;
+`;
 
 const Main = () => {
-    return (
-      <div className="main-content">
-          <HomePage />
-          <AboutPage />
-          <ExperiencePage />
-          <WorkPage />
-          <ContactPage />
-      </div>
-    );
-  };
-  
-  export default Main;
+  return (
+    <StyledMain>
+      <HomePage />
+      <AboutPage />
+      <ExperiencePage />
+      <WorkPage />
+      <ContactPage />
+    </StyledMain>
+  );
+};
+
+export default Main;
