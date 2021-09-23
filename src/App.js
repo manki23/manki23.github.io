2a4622b7a774a03ab9d619f5e1132c3e184836b7
Myriam <manki@student.42.fr>
2021-09-23 16:22:28 +0200

    MOD: Footer.js & App.js > styled-components instead of scss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,106 @@
-import './App.scss';
-import LeftFooter from './components/shared/LeftFooter';
-import Header from './components/shared/Header';
-import Logo from './components/shared/Logo';
-import Main from './components/pages/Main';
-import Footer from './components/shared/Footer';
+import styled, { ThemeProvider } from "styled-components";
+import LeftFooter from "./components/shared/LeftFooter";
+import Header from "./components/shared/Header";
+import Logo from "./components/shared/Logo";
+import Main from "./components/pages/Main";
+import Footer from "./components/shared/Footer";
+
+const theme = {
+  colors: {
+    appNavyColor: `#0a192f`,
+    appLightNavyColor: `#112240`,
+    appLightestNavyColor: `#233554`,
+    appSlateColor: `#8892b0`,
+    appLightSlateColor: `#a8b2d1`,
+    appLightestSlateColor: `#ccd6f6`,
+    appWhiteColor: `#e6f1ff`,
+    appGreenColor: `#64ffda`,
+    appGreenTextBackgroundColor: `#133141`,
+  },
+  fontMono: `"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace`,
+  transition: `all 0.25s cubic-bezier(0.645,0.045,0.355,1)`,
+};
+
+const StyledApp = styled.div`
+  * {
+    background-color: ${(props) => props.theme.colors.appNavyColor};
+    box-sizing: border-box;
+  }
+
+  counter-set: headerCounter 5 h2Counter 0;
+  color: ${(props) => props.theme.colors.appWhiteColor};
+  margin: 10px;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    transition: ${(props) => props.theme.transition};
+    cursor: pointer;
+  }
+
+  h1,
+  h2,
+  h3 {
+    font-weight: 600;
+    color: ${(props) => props.theme.colors.appLightestSlateColor};
+  }
+
+  h2 {
+    line-height: 1.1;
+  }
+
+  h3 {
+    font-size: 22px;
+  }
+
+  div {
+    font-size: 1.1em;
+  }
+
+  p,
+  ol,
+  li {
+    color: $appSlateColor;
+    font-size: 1.1em;
+  }
+
+  .numbered-heading::before {
+    position: relative;
+    counter-increment: h2Counter 1;
+    content: "0" counter(h2Counter) ".";
+    margin-right: 10px;
+    color: ${(props) => props.theme.colors.appGreenColor};
+    font-family: ${(props) => props.theme.fontMono};
+    font-size: 3vw;
+    font-weight: 400;
+  }
+
+  .numbered-heading {
+    display: flex;
+    align-items: center;
+    position: relative;
+    margin: 10px 0px 40px;
+    width: 100%;
+    font-size: 3vw;
+    white-space: nowrap;
+  }
+
+  .slate-color {
+    color: ${(props) => props.theme.colors.appSlateColor};
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Logo />
-      <Header />
-      <Main />
-      <LeftFooter />
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        <Logo />
+        <Header />
+        <Main />
+        <LeftFooter />
+        <Footer />
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,18 +1,34 @@
 import React from "react";
-import '../../styles/shared/Footer.scss';
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  bottom: 0;
+  width: 70vw;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 25px;
+  p {
+    font-family: monospace;
+    text-align: center;
+    a {
+      color: $appGreenColor;
+    }
+  }
+`;
 
 const Footer = () => {
   return (
-    <footer className="footer-container">
+    <StyledFooter>
       <p>
         Disclaimer: I did not create the design of this website. <br /> It was
         inspired by{" "}
         <a target="_blank" href="https://brittanychiang.com/">
           Brittany Chiang portfolio
         </a>
-        .<br />If you liked this design, please check out her work.
+        .<br />
+        If you liked this design, please check out her work.
       </p>
-    </footer>
+    </StyledFooter>
   );
 };
 
