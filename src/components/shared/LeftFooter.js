1bf9221178362aa5a42dc2242c5f2a3a06915002
Myriam <manki@student.42.fr>
2021-09-23 16:40:29 +0200

    MOD: LeftFooter.js > use of styled-components instead of scss

--- src/components/shared/LeftFooter.js
+++ src/components/shared/LeftFooter.js
@@ -1,21 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { FaRegEnvelope } from "react-icons/fa";
-import "../../styles/shared/LeftFooter.scss";
+
+const StyledLeftFooter = styled.div`
+  position: fixed;
+  left: 40px;
+  bottom: 0;
+
+  p:hover {
+    color: ${(props) => props.theme.colors.appGreenColor};
+  }
+
+  p {
+    font-size: 23px;
+    margin: 20px 0px;
+  }
+
+  p:active {
+    outline: 2px dashed ${(props) => props.theme.colors.appGreenColor};
+    outline-offset: 6px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 0.5em;
+    min-height: 6em;
+    border-right: 1px solid ${(props) => props.theme.colors.appSlateColor};
+  }
+`;
 
 const LeftFooter = () => {
   return (
-    <div className="left-footer">
-      <p className="left-footer-icon">
-        <a
-          href="https://github.com/manki23"
-          target="_blank"
-          title="GitHub"
-        >
+    <StyledLeftFooter>
+      <p>
+        <a href="https://github.com/manki23" target="_blank" title="GitHub">
           <FiGithub />
         </a>
       </p>
-      <p className="left-footer-icon">
+      <p>
         <a
           href="https://www.linkedin.com/in/myriam-anki-8b9258198/"
           target="_blank"
@@ -24,17 +47,13 @@ const LeftFooter = () => {
           <FiLinkedin />
         </a>
       </p>
-      <p className="left-footer-icon">
-        <a
-          href="mailto:myriam.anki.pro@gmail.com"
-          target="_blank"
-          title="Mail"
-        >
+      <p>
+        <a href="mailto:myriam.anki.pro@gmail.com" target="_blank" title="Mail">
           <FaRegEnvelope />
         </a>
       </p>
-      <span className="left-line"></span>
-    </div>
+      <span />
+    </StyledLeftFooter>
   );
 };
 
