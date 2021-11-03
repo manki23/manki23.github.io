e4f5ab1484092d2ea3708afad12b8ce96f911937
Myriam <manki@student.42.fr>
2021-11-03 19:42:33 +0100

    SAN: ChatHeader.js > imports

--- src/components/ChatComponent/ChatHeader.js
+++ src/components/ChatComponent/ChatHeader.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import logo from "../../assets/manki-bitmoji.png";
 import { MdClose } from "react-icons/md";
